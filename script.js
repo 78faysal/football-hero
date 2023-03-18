59fb9993978944ecf02605a05801b399fc70f4be
mdfaysal <faysal7830@gmail.com>
2023-03-18 20:43:01 +0600

    js file added

---
+++ script.js
@@ -0,0 +1,8 @@
+const cardContainer = document.getElementsByClassName('selected-card')
+const selectButton = document.getElementsByClassName('selectBtn')
+const cardCollection = document.getElementsByClassName('card');
+
+function handleOnClick() {
+    const cardValue = cardCollection
+    console.log(cardValue)
+}
