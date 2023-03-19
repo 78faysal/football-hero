504f40868feaccecf8b70ce1ca9274927736fcbe
mdfaysal <faysal7830@gmail.com>
2023-03-20 00:58:10 +0600

    all done

--- script.js
+++ script.js
@@ -7,13 +7,30 @@ Object.keys(cardBtns).forEach((key, index) => {
         const li = document.createElement('li');
         li.textContent = title;
         cardList.appendChild(li);
-        
+
+        cardBtns[key].disabled = true;
+        cardBtns[key].style.backgroundColor = 'gray';
     });
 });
 
 
 
 document.getElementById('player-expenses').addEventListener('click', function () {
-    const playerValue = document.getElementById('player-expenses-input').value;
-    console.log(playerValue)
+    const playerInput = document.getElementById('player-expenses-input');
+    const playerValue = Number(playerInput.value);
+    const totalPlayer = cardList.children.length;
+    const expenses = document.getElementById('expensesOfTotalPlayer');
+    expenses.textContent = playerValue * totalPlayer;
+})
+
+
+document.getElementById('calculateTotal').addEventListener("click", () =>{
+    const managerValue = parseInt(document.getElementById('managerValue').value);
+    const coachValue = parseInt(document.getElementById('coachValue').value);
+    const playersValue = parseInt(document.getElementById('expensesOfTotalPlayer').textContent);
+    const totalValue = managerValue + playersValue + coachValue ;
+    console.log(totalValue);
+    var totalValueInput = document.getElementById('totalValue');
+    totalValueInput.textContent = totalValue;
+    console.log(totalValue)
 })
